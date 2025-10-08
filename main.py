f5bd087738be74b61f17d2f71ba37ac91cd71b40
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2025-10-08 21:29:58 +0200

    3

--- main.py
+++ main.py
@@ -17,12 +17,17 @@ calculate_person_age("2000-01-01") #25
 calculate_person_age("2025-10-10") #25
 
 def get_days_from_today(date_str: str)-> int:
-    date = datetime.strptime(date_str, "%Y-%m-%d").date() #str parse date
-    print (date)
-    today = datetime.today().date()
-    print(today)
-    resalt= (today - date).days
-    print(resalt)
-    return resalt
-get_days_from_today('2020-10-09')
-get_days_from_today('2025-12-12')
+    try:
+        date = datetime.strptime(date_str, "%Y-%m-%d").date() #str parse date
+        today = datetime.today().date()
+        resalt= (today - date).days
+        print(resalt)
+        return resalt
+    except ValueError:
+        print("Помилка: Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'.")
+        return None
+print(get_days_from_today('2020-10-09'))
+print(get_days_from_today('2025-12-12'))
+print(get_days_from_today('09-10-2020'))  
+print(get_days_from_today('2020/10/09'))  
+print(get_days_from_today('2020-13-01'))  
